b2e5e97bf675360054c57d747b9f191a14e2dc1c
Luca-Spreafico <161072594+Luca4Spreafico@users.noreply.github.com>
2025-09-22 22:17:05 +0200

    version 0.9

--- attempt1.cpp
+++ attempt1.cpp
@@ -1,14 +1,14 @@
-//#include <Eigen/Dense>
+#include <Eigen/Dense>
 #include <iostream>
 #include <cstdlib>
 
 // from https://github.com/nothings/stb/tree/master
-//#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION
 
 #include "stb_image.h"
 
-//#define STB_IMAGE_WRITE_IMPLEMENTATION
-//#include "stb_image_write.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
 
 using namespace Eigen;
 
